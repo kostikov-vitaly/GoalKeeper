420d645ecbec1841cb22a03454d537eb84eec3c1
Vitaly Kostikov <one.kostikov@gmail.com>
2021-12-18 21:08:35 +0100

    Initial Commit

---
+++ GoalKeeper/GoalKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalKeeperApp.swift
+//  GoalKeeper
+//
+//  Created by Vitaly on 18/12/21.
+//
+
+import SwiftUI
+
+@main
+struct GoalKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
